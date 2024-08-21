e41eeb81945ee3f9d5b895c30e283a124ea3ebfa
Carddera1842 <carddera1842@gmail.com>
2024-08-20 21:06:50 -0500

    GA 6

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import styles from "./styling.css"
+
+export default function Banner() {
+    return(
+        <>
+        <header className="header">
+            <h1>Orbit Report</h1>
+        </header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+        </>
+    );
+}
